e07f606be58c8ad51774fa49d750cea883a61688
Dylan <Dylanwinn09@gmail.com>
2023-03-06 18:30:16 -0700

    Feature 8 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from projects.models import Project
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def list_projects(request):
-    projects = Project.objects.all()
+    projects = Project.objects.filter(owner=request.user)
     context = {
         "all_projects": projects,
     }
     return render(request, "projects/project_list.html", context)
 
 
+@login_required
 def redirect_to_home(request):
     return redirect("list_projects")
